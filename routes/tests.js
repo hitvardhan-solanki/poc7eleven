27f1e41ab9e8c6ca1377bfd9c8e50372837cce7a
Hitvardhan Solanki <hitvardhansolanki@Hitvardhans-MacBook-Pro.local>
2017-05-05 13:01:23 +0530

    V commit

--- routes/tests.js
+++ routes/tests.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.end("sample test");
+    res.render('test', { title: 'tests' });
 });
 
 module.exports = router;
