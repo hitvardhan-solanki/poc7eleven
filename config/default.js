b7ab381f7427678e2eb848c79ce835df9c793745
Kapil-pc <kapil.nainani@appirio.com>
2017-05-06 14:47:12 +0530

    2 run

---
+++ config/default.js
@@ -0,0 +1,7 @@
+module.exports = {
+  PORT: process.env.PORT,
+    DBPORT: process.env.DBPORT,
+    USERNAME: process.env.USERNAME,
+    DBNAME: process.env.DBNAME,
+    PASSWORD: process.env.PASSWORD,
+}
