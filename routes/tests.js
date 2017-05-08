123805c8fcceebc70a5a6d262844e3abfeebff6c
hitvardhan Solanki <hitvardhan.solanki@appirio.com>
2017-05-08 20:01:10 +0530

    test Account

--- routes/tests.js
+++ routes/tests.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 var testServices = require('../helpers/testServices');
 
-router.get('/account/:limit',testServices.getAccounts);
+//router.get('/account/:limit',testServices.getAccounts);
 router.render('account/:limit',{listOfRecords: testServices.getAccounts})
 
 
